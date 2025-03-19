ba5c05ffc7ae06f7491d0aabbf3be9824aaa1dfd
Pedro Miranda <mirandasupley@gmail.com>
2025-03-19 16:38:43 -0300

    Alterações

---
+++ src/estudo/segundasemana/finalprojetolistcolecaodados/Cartao.java
@@ -0,0 +1,17 @@
+package estudo.segundasemana.finalprojetolistcolecaodados;
+
+public class Cartao {
+    private double limite;
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public Cartao (Double limite) {
+        this.limite = limite;
+    }
+}
